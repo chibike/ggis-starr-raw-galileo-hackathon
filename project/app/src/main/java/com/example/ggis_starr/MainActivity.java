f45beaaad3b0b72f3e1cb89b686026afc47401d9
Chibuike Okpaluba <chibuikepraise@gmail.com>
2020-03-21 14:12:15 +0000

    added the flamingo API

--- project/app/src/main/java/com/example/ggis_starr/MainActivity.java
+++ project/app/src/main/java/com/example/ggis_starr/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
+import flamingo.flamingo_api.FlamingoManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FlamingoManager flamingoManager = new FlamingoManager(MainActivity.this, new ArrayList<>());
     }
 }
